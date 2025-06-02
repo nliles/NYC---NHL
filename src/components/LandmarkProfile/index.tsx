2dbc5a43b1cd7fea299e9e4ce1f002b14b3b74dd
Natalie Liles <natalie.liles@mavenclinic.com>
2025-06-02 18:07:38 -0400

    update

--- src/components/LandmarkProfile/index.tsx
+++ src/components/LandmarkProfile/index.tsx
@@ -1,3 +1,4 @@
+import { saveToStorage } from "../../helpers/localStorage";
 import styles from "./LandmarkProfile.module.css";
 
 const LandmarkProfile = ({
@@ -15,20 +16,11 @@ const LandmarkProfile = ({
     // Save to localStorage
     if (isVisited) {
       setVisitedLandmarks([...visitedLandmarks, landmark.id]);
-      localStorage.setItem(
-        "visitedLandmarks",
-        JSON.stringify([...visitedLandmarks, landmark.id]),
-      );
+      saveToStorage([...visitedLandmarks, landmark.id]);
     } else {
-      setVisitedLandmarks(
-        visitedLandmarks.filter((id: any) => id !== landmark.id),
-      );
-      localStorage.setItem(
-        "visitedLandmarks",
-        JSON.stringify(
-          visitedLandmarks.filter((id: any) => id !== landmark.id),
-        ),
-      );
+      const filtered = visitedLandmarks.filter((id: any) => id !== landmark.id);
+      setVisitedLandmarks(filtered);
+      saveToStorage(JSON.stringify(filtered));
     }
   };
 
