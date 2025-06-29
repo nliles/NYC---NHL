f4a3c0c0e428a6baceb15e18680129446a4576f9
Natalie Liles <natalie.liles@mavenclinic.com>
2025-06-29 15:25:13 -0400

    add about panel

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,26 +4,24 @@ import styles from "./NavBar.module.css";
 
 const NavBar = ({ count, total }: { count: number; total: number }) => {
   const [showAbout, setShowAbout] = useState(false);
-  const toggleAbout = () => setShowAbout(prev => !prev);
+  const toggleAbout = () => setShowAbout((prev) => !prev);
   return (
-  <>
-    <nav className={styles.nav}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>National Historic Landmarks of NYC</h1>
-        <div className={styles.rightContent}>
-        <div className={styles.progress}>
-          <img src="progress.svg" />
-          <span>{`${count} out of ${total}`}</span>
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>National Historic Landmarks of NYC</h1>
+          <div className={styles.rightContent}>
+            <div className={styles.progress}>
+              <img src="progress.svg" />
+              <span>{`${count} out of ${total}`}</span>
+            </div>
+            <button className={styles.buttonLink} onClick={toggleAbout}>
+              About
+            </button>
+          </div>
         </div>
-        <button className={styles.buttonLink} onClick={toggleAbout}>About</button>
-        </div>
-      </div>
-    </nav>
-    {showAbout && (
-          <SidePanel onClose={toggleAbout}>
-            ADD CONTENT
-          </SidePanel>
-    )}
+      </nav>
+      {showAbout && <SidePanel onClose={toggleAbout}>ADD CONTENT</SidePanel>}
     </>
   );
 };
