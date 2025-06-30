91095b9b2a721d1c3fc5d9672b6e9c7b53c311a8
Natalie Liles <natalie.liles@mavenclinic.com>
2025-06-30 16:24:19 -0400

    fix styles

--- src/components/LandmarkList/index.tsx
+++ src/components/LandmarkList/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import cn from "classnames";
 import styles from "./LandmarkList.module.scss";
 import { Landmark } from "../../types";
 import SearchBar from "../SearchBar";
+import MobileDrawer from "../MobileDrawer";
 const boroughs = [
   "All",
   "Manhattan",
@@ -61,49 +62,60 @@ const LandmarkList = ({
       ? `Showing ${landmarks.length} landmarks`
       : `Showing ${filteredLandmarks.length} of ${landmarks.length} landmarks`;
 
-  return (
-    <div className={styles.leftPanel}>
-      <div className={styles.panelHeader}>
-        <SearchBar onChange={handleOnChange} />
-        <div className={styles.boroughsContainer}>
-          {boroughs.map((borough) => (
-            <button
-              className={cn(styles.boroughBtn, {
-                [styles.selected]: selectedBorough === borough,
-              })}
-              onClick={() => setSelectedBorough(borough)}
-            >
-              {borough}
-            </button>
-          ))}
-        </div>
-      </div>
-      <div
-        className={styles.resultsCount}
-        aria-label="results-count"
-        id="resultsCount"
-      >
-        {filterCopy}
-      </div>
-      <ul className={styles.landmarksList}>
-        {filteredLandmarks.map((landmark: Landmark) => (
-          <li>
-            <button
-              className={styles.item}
-              onClick={() => handleClick(landmark)}
-              key={landmark.fields.name}
-            >
-              <p className={styles.name}>{landmark.fields.name}</p>
-              <div className={styles.landmarkDetails}>
-                {landmark.fields.borough.split(",").map((borough: string) => (
-                  <span className={styles.borough}>{borough}</span>
-                ))}
-              </div>
-            </button>
-          </li>
+  const getListPanel = () => (
+    <>
+    <div className={styles.panelHeader}>
+      <SearchBar onChange={handleOnChange} />
+      <div className={styles.boroughsContainer}>
+        {boroughs.map((borough) => (
+          <button
+            className={cn(styles.boroughBtn, {
+              [styles.selected]: selectedBorough === borough,
+            })}
+            onClick={() => setSelectedBorough(borough)}
+          >
+            {borough}
+          </button>
         ))}
-      </ul>
+      </div>
     </div>
+    <div
+      className={styles.resultsCount}
+      aria-label="results-count"
+      id="resultsCount"
+    >
+      {filterCopy}
+    </div>
+    <ul className={styles.landmarksList}>
+      {filteredLandmarks.map((landmark: Landmark) => (
+        <li>
+          <button
+            className={styles.item}
+            onClick={() => handleClick(landmark)}
+            key={landmark.fields.name}
+          >
+            <p className={styles.name}>{landmark.fields.name}</p>
+            <div className={styles.landmarkDetails}>
+              {landmark.fields.borough.split(",").map((borough: string) => (
+                <span className={styles.borough}>{borough}</span>
+              ))}
+            </div>
+          </button>
+        </li>
+      ))}
+    </ul>
+  </>
+  )
+
+  return (
+  <>
+  <div className={styles.leftPanel}>
+    {getListPanel()}
+  </div>
+    <MobileDrawer>
+        {getListPanel()}
+    </MobileDrawer>
+    </>
   );
 };
 
