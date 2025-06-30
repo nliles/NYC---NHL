90ca04dfdc1d2f38a74dcc1b98d2684f47297758
Natalie Liles <natalie.liles@mavenclinic.com>
2025-06-30 13:07:50 -0400

    fix header on mobile

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,12 @@
 import styles from "./NavBar.module.css";
+import cn from "classnames";
+
+const Progress = ({ count, total, className } : { count: number; total: number; className?: string }) => (
+  <div className={cn(styles.progress, className)}>
+  <img src="progress2.svg" width="45px" height="auto" />
+  <span>{`${count} out of ${total}`}</span>
+</div>
+)
 
 const NavBar = ({
   count,
@@ -13,12 +21,12 @@ const NavBar = ({
     <>
       <nav className={styles.nav}>
         <div className={styles.content}>
+          <div className={styles.leftContent}>
           <h1 className={styles.title}>National Historic Landmarks of NYC</h1>
+          <Progress count={count} total={total} className={styles.mobile}/>
+          </div>
           <div className={styles.rightContent}>
-            <div className={styles.progress}>
-              <img src="progress2.svg" width="45px" height="auto" />
-              <span>{`${count} out of ${total}`}</span>
-            </div>
+            <Progress count={count} total={total} className={styles.desktop}/>
             <button className={styles.buttonLink} onClick={toggleAbout}>
               About
             </button>
