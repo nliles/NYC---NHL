bc0e85eef35178d301de287d787451212dfc1d68
Natalie Liles <natalie.liles@mavenclinic.com>
2025-07-06 12:02:23 -0400

    update swipe gestures

--- src/components/MobileDrawer/index.tsx
+++ src/components/MobileDrawer/index.tsx
@@ -8,11 +8,22 @@ const MobileDrawer = ({ children }: { children: React.ReactNode }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const handlers = useSwipeable({
-    onSwipedUp: () => setIsExpanded(true),
-    onSwipedDown: () => setIsExpanded(false),
+    onSwipedUp: (eventData) => {
+      eventData.event.stopPropagation();
+      setIsExpanded(true);
+    },
+    onSwipedDown: (eventData) => {
+      eventData.event.stopPropagation();
+      setIsExpanded(false);
+    },
+    onSwiping: (eventData) => {
+      eventData.event.stopPropagation();
+      eventData.event.preventDefault();
+    },
     trackMouse: true,
     delta: 30,
     trackTouch: true,
+    preventScrollOnSwipe: true,
   });
 
   const handleHeaderClick = () => {
