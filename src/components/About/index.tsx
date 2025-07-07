7a075aeb821da38ca75e65bafaa9a7fc502e84da
Natalie Liles <natalie.liles@mavenclinic.com>
2025-07-07 18:01:44 -0400

    update about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -33,9 +33,8 @@ const About = () => (
           >
             National Historic Landmarks (NHLs)
           </a>{" "}
-          are historic properties designated by the Secretary of the Interior
-          for their exceptional value in illustrating American heritage and
-          culture.
+          are properties designated by the Secretary of the Interior for their
+          exceptional value in illustrating American heritage and culture.
         </p>
         <p>
           New York City boasts 116 National Historic Landmarks, more than any
