690c9a66f25b596d8b68218b123f2d6d0f491000
Natalie Liles <natalie.liles@mavenclinic.com>
2025-10-05 15:15:30 -0400

    update grade

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { patchCssModules } from "vite-css-modules";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [patchCssModules(), react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "src/styles/_colors.scss";`,
+        api: 'modern-compiler',
+        additionalData: `@use "src/styles/_colors.scss" as *;`,
       },
     },
   },
