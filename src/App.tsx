95e5d90bcf210bd14fd1e62cc6063dad476b49e8
Natalie Liles <natalie.liles@mavenclinic.com>
2025-12-27 11:26:57 -0500

    clean up fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import LandmarkFinder from "./components/LandmarkFinder";
+import "./styles/fonts.scss";
 import "./App.css";
 
 function App() {
